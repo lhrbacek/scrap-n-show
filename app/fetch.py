b57db6b2bb040fa582a3bb8cfb371ed7449d31f9
Lubomir Hrbacek <lubomirhrbacek@seznam.cz>
2022-09-18 20:15:47 +0200

    Rename files and add app folder

--- app/fetch.py
+++ app/fetch.py
@@ -3,12 +3,12 @@ import json
 import psycopg2
 
 def fetch_data():
-    flats_json = urlopen('https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&per_page=20&tms=1663433765480')
+    flats_json = urlopen('https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&per_page=5000&tms=1663433765480')
     flats = json.loads(flats_json.read().decode("utf-8"))
 
     flats_db = []
-    for i in range(20):
-        flats_db.append((flats["_embedded"]["estates"][i]["name"], flats["_embedded"]["estates"][i]["_links"]["images"][0]["href"]))
+    for i in range(500):
+        flats_db.append((flats["_embedded"]["estates"][i]["name"].replace('\xa0', ' '), flats["_embedded"]["estates"][i]["_links"]["images"][0]["href"]))
     return flats_db
 
 def connect_create_fill(flats_db):
@@ -18,7 +18,7 @@ def connect_create_fill(flats_db):
         img_url VARCHAR(255)
     )
     """
-    insert_sql = "INSERT INTO flats_info (title, img_url) VALUES(%s, %s);"
+    insert_sql = "INSERT INTO flats_info (title, img_url) VALUES(%s, %s)"
 
     conn = None
     try:
@@ -31,14 +31,15 @@ def connect_create_fill(flats_db):
         cur = conn.cursor()
         cur.execute(create_sql)
         cur.executemany(insert_sql, flats_db)
-
         cur.close()
         conn.commit()
+
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
 
-flats_db = fetch_data()
-connect_create_fill(flats_db)
+if __name__ == "__main__":
+    flats_db = fetch_data()
+    connect_create_fill(flats_db)
